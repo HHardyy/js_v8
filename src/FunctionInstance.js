3d01eca778d7c519b9a406cdd3a5572be47ee9b6
hardy <bingzhenmail@gmail.com>
2021-12-22 23:54:41 +0800

    实现闭包原理，浏览器内多Scops

--- src/FunctionInstance.js
+++ src/FunctionInstance.js
@@ -1,18 +1,19 @@
 /*
  * @Author: 小方块 
  * @Date: 2021-12-21 18:38:17 
- * @Last Modified by:   小方块 
- * @Last Modified time: 2021-12-21 18:38:17 
+ * @Last Modified by: 小方块
+ * @Last Modified time: 2021-12-22 23:53:00
  * 
  * 函数
  * 
  */
 
 class FunctionInstance {
-  constructor(name, code, scope) {
+  constructor(name, code, scope, Scopes) {
     this.name = name
     this.code = code
     this.scope = scope
+    this.Scopes = Scopes
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
  * @Author: 小方块 
  * @Date: 2021-12-21 16:30:35 
  * @Last Modified by: 小方块
- * @Last Modified time: 2021-12-22 22:27:03
+ * @Last Modified time: 2021-12-22 23:52:42
  * 
  * 入口文件 
  */
@@ -49,60 +49,44 @@ let testFnExcutionContext = new ExecutionContext(testFnLexicalEnvironment, globa
 ESCstack.push(testFnExcutionContext)
 ESCstack.current.LexicalEnvironment.createBinding('b')
 ESCstack.current.LexicalEnvironment.setBinding('b', undefined)
+
+// 1、声明一个函数testFn1 (一个词法环境)
+// let testFn1 = new FunctionInstance(
+//   'testFn1',
+//   'console.log(a, b)',
+//   ESCstack.current.LexicalEnvironment
+// )
+
+// 2、声明一个函数testFn1 多个词法环境 （浏览器内）
+let Scopes = []
+let currentLexicalEnvironment = ESCstack.current.LexicalEnvironment
+while (currentLexicalEnvironment) {
+  Scopes.push(currentLexicalEnvironment)
+  currentLexicalEnvironment = currentLexicalEnvironment.outer
+}
+let testFn1 = new FunctionInstance(
+  'testFn1',
+  'console.log(a, b)',
+  ESCstack.current.LexicalEnvironment,
+  Scopes
+)
+
+ESCstack.current.LexicalEnvironment.createBinding('testFn1')
+ESCstack.current.LexicalEnvironment.setBinding('testFn1', testFn1)
 // 开始执行
 ESCstack.current.LexicalEnvironment.setBinding('b', 2)
 
-// console.log(
-//   ESCstack.current.LexicalEnvironment.GetIdentifierReference('a'),
-//   ESCstack.current.LexicalEnvironment.GetIdentifierReference('b')
-// )
-
-
-/**
- * =======================================================================================
- * 如果遇到新的代码块， 创建一个新词法环境对象， 让新的词法环境对象.outer = 当前的词法环境
- *
- * 开始执行块级代码
- * 只有全局函数才会创建新的执行上下文
- * 块级作用域不会生成新的执行上下文，但是会生成新的词法环境
-*/
-let oldEnv = ESCstack.current.LexicalEnvironment
-// 以oldenv为父级词法环境创建词法环境
-let block1Env = LexicalEnvironment.NewDeclearactiveEnvironment(oldEnv)
-// block1的变量提升阶段
-block1Env.createBinding('c')
-block1Env.setBinding('c', { type: 'let', uninitialized: true })
-// 让当前执行上下文的词法环境对象指向block1Env
-ESCstack.current.LexicalEnvironment = block1Env
-
-// block1Env.getBindingValue("c")
-block1Env.setBinding('c', 3)
-// console.log(
-//   ESCstack.current.LexicalEnvironment.GetIdentifierReference('a'),
-//   ESCstack.current.LexicalEnvironment.GetIdentifierReference('b'),
-//   ESCstack.current.LexicalEnvironment.GetIdentifierReference('c')
-// )
-
-/**
- * 执行完block1之后再退回上一个执行环境
-*/
-ESCstack.current.LexicalEnvironment = oldEnv
-
-let block2Env = LexicalEnvironment.NewDeclearactiveEnvironment(oldEnv)
-block2Env.createBinding('c')
-block2Env.setBinding('c', { type: 'let', uninitialized: true })
-ESCstack.current.LexicalEnvironment = block2Env
-block2Env.setBinding('c', 4)
-console.log(
-  ESCstack.current.LexicalEnvironment.GetIdentifierReference('a'),
-  ESCstack.current.LexicalEnvironment.GetIdentifierReference('b'),
-  ESCstack.current.LexicalEnvironment.GetIdentifierReference('c')
-)
-/**
- * 执行完block1之后再退回上一个执行环境
-*/
-ESCstack.current.LexicalEnvironment = oldEnv
-// 弹出testFn执行上下文
+// testFn出栈
 ESCstack.pop()
 
-// 然后那些block就被gc了~
+// 创建一个testFn1的词法环境
+let testFn1LexicalEnvironment = LexicalEnvironment.NewDeclearactiveEnvironment(testFn1.scope)
+// 创建一个testFn1的执行上下文
+let testFn1ExcutionContext = new ExecutionContext(testFn1LexicalEnvironment, global)
+// testFn1入栈
+ESCstack.push(testFn1ExcutionContext)
+// 执行testFn1
+console.log(
+  ESCstack.current.LexicalEnvironment.GetIdentifierReference('a'),
+  ESCstack.current.LexicalEnvironment.GetIdentifierReference('b')
+)
